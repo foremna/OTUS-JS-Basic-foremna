6fab0dcc682c545b2852eba66853ab04d8341c3e
Lada Penkova <lada.html@gmail.com>
2020-11-22 03:35:02 +0300

    Обернула программы в функции

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 "use strict";
 
+// Lesson 1. Homework 1.
 // 1.В переменных a и b хранятся числа. Написать
 // программу, которая выводит в консоль произведение
 // и сумму этих чисел
 
-let a = 5;
-let b = 20;
+function sumNumbers () {
+    let a = 5;
+    let b = 20;
 
-console.log(a * b);
-console.log(a + b);
+    console.log(a * b);
+    console.log(a + b);
+}
 
+sumNumbers();
 
 // 2.В двух переменных хранятся строки символов.
 // Написать программу, которая выведет в консоль
 // суммарное количество символов в обоих строках.
 
-let str1 = "Hel";
-let str2 = "lo";
+function concatStr () {
+    let str1 = "Hel";
+    let str2 = "lo";
 
-console.log(str1 + str2);
+    console.log(str1 + str2);
+}
+
+concatStr();
+
